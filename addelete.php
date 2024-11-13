e3cb7c2d1ebff529b02ccfd15b724682fbc9e502
RamuCS <116806353+RamuCS@users.noreply.github.com>
2024-11-13 12:37:50 +0530

    Delete addelete.php

--- addelete.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); ?>
-<?php
-include("db.php");
-$id = $_GET['id'];
-$result=mysqli_query($mysqli, "DELETE FROM predefined_tasks WHERE id=$id  ");
-$result=mysqli_query($mysqli,"DELETE FROM tasks where id = $id");
-header("Location:addpretask.php");
-?>
-
